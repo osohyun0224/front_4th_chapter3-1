c3e65524f8864448be0648574cb244b274337f0b
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2025-02-02 22:31:37 +0900

    ✅ tests(notificationUtils) :: createNotificationMessage, getUpcomingEvents 함수에 대한 테스트 코드 작성

--- src/__tests__/unit/easy.notificationUtils.spec.ts
+++ src/__tests__/unit/easy.notificationUtils.spec.ts
@@ -1,16 +1,77 @@
 import { Event } from '../../types';
-import { createNotificationMessage, getUpcomingEvents } from '../../utils/notificationUtils';
+
+import {
+  createNotificationMessage,
+  getUpcomingEvents,
+} from '../../utils/notificationUtils';
+
+const events: Event[] = [
+  {
+    id: '1',
+    title: '해리와 과제하기',
+    date: '2025-02-04',
+    startTime: '18:00',
+    endTime: '22:00',
+    description: '항해 플러스 7주차 과제하기',
+    location: '스파크플러스',
+    category: '개인',
+    repeat: { type: 'none', interval: 0 },
+    notificationTime: 30,
+  },
+  {
+    id: '2',
+    title: '정원이랑 놀기',
+    date: '2025-02-24',
+    startTime: '15:00',
+    endTime: '18:00',
+    description: '정원이랑 한 주에 2일은 놀기',
+    location: '성수, 뚝섬',
+    category: '개인',
+    repeat: { type: 'none', interval: 0 },
+    notificationTime: 30,
+  },
+];
 
 describe('getUpcomingEvents', () => {
-  it('알림 시간이 정확히 도래한 이벤트를 반환한다', () => {});
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
 
-  it('이미 알림이 간 이벤트는 제외한다', () => {});
+  afterEach(() => {
+    vi.useRealTimers();
+  });
 
-  it('알림 시간이 아직 도래하지 않은 이벤트는 반환하지 않는다', () => {});
+  it('알림 시간이 정확히 도래한 이벤트를 반환한다', () => {
+    const date = new Date('2025-02-04T17:30:00');
+    const upcomingEvents = getUpcomingEvents(events, date, []);
+    expect(upcomingEvents).toHaveLength(1);
+  });
 
-  it('알림 시간이 지난 이벤트는 반환하지 않는다', () => {});
+  it('이미 알림이 간 이벤트는 제외한다', () => {
+    const date = new Date('2025-02-04T17:30:00');
+    vi.setSystemTime(date);
+    const upcomingEvents = getUpcomingEvents(events, new Date(), ['1']);
+    expect(upcomingEvents).toHaveLength(0);
+  });
+
+  it('알림 시간이 아직 도래하지 않은 이벤트는 반환하지 않는다', () => {
+    const date = new Date('2025-02-04T17:00:00');
+    vi.setSystemTime(date);
+    const upcomingEvents = getUpcomingEvents(events, new Date(), []);
+    expect(upcomingEvents).toHaveLength(0);
+  });
+
+  it('알림 시간이 지난 이벤트는 반환하지 않는다', () => {
+    const date = new Date('2025-02-04T18:30:00');
+    vi.setSystemTime(date);
+    const upcomingEvents = getUpcomingEvents(events, new Date(), []);
+    expect(upcomingEvents).toHaveLength(0);
+  });
 });
 
 describe('createNotificationMessage', () => {
-  it('올바른 알림 메시지를 생성해야 한다', () => {});
+  it('올바른 알림 메시지를 생성해야 한다', () => {
+    const message = createNotificationMessage(events[0]);
+    expect(message).toBe('30분 후 해리와 과제하기 일정이 시작됩니다.');
+  });
 });
