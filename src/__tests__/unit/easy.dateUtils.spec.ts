dc5d7ffa5686db04ea181f69bcf76a2427279b0c
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2025-02-02 21:46:36 +0900

    ✅ tests(dateUtils) :: formatMonth 함수 테스트 코드 작성

--- src/__tests__/unit/easy.dateUtils.spec.ts
+++ src/__tests__/unit/easy.dateUtils.spec.ts
@@ -263,7 +263,10 @@ describe('formatWeek', () => {
 });
 
 describe('formatMonth', () => {
-  it("2024년 7월 10일을 '2024년 7월'로 반환한다", () => {});
+  it("2024년 7월 10일을 '2024년 7월'로 반환한다", () => {
+    const month = formatMonth(new Date('2024-07-10'));
+    expect(month).toBe('2024년 7월');
+  });
 });
 
 describe('isDateInRange', () => {
