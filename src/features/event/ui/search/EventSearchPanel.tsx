5c4af47e63f338ca57f5715ad7ac72e7051ad135
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2025-02-03 18:05:15 +0900

    ♻️ refactor :: Search에 의존하는 컴포넌트 리팩토링

---
+++ src/features/event/ui/search/EventSearchPanel.tsx
@@ -0,0 +1,26 @@
+import { FormControl, FormLabel, Input, VStack } from '@chakra-ui/react';
+
+import EventSearchResults from "@/features/event/ui/search/EventSearchResults"
+import { useEventContext } from '@/features/event/model/useEventContext';
+
+export const EventSearchPanel = () => {
+  const { searchValues, notificationsValues } = useEventContext();
+
+  const { searchTerm, filteredEvents, setSearchTerm } = searchValues;
+  const { notifiedEvents } = notificationsValues;
+
+  return (
+    <VStack data-testid="event-list" w="500px" h="full" overflowY="auto">
+      <FormControl>
+        <FormLabel>일정 검색</FormLabel>
+        <Input
+          placeholder="검색어를 입력하세요"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </FormControl>
+
+      <EventSearchResults filteredEvents={filteredEvents} notifiedEvents={notifiedEvents} />
+    </VStack>
+  );
+};
